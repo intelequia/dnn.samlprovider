c80e4bd2fe9bd8f120f2b4079154139b12c1ef27
David J. Rodriguez Hernández <davidj@intelequia.com>
2025-05-20 19:23:47 +0100

    feat: preparation for v1.0.2 release

--- src/DotNetNuke.Authentication.SAML/Properties/AssemblyInfo.cs
+++ src/DotNetNuke.Authentication.SAML/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("854c2357-7108-4216-bd0f-ff61d3665703")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
